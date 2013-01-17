8dfa5ebf796917c5aa59da2665e5b43c66c74b6c
Viesturs Silins <silins.viesturs@gmail.com>
2013-01-17 18:17:26 +0200

    Typo fix

--- pycps/connection.py
+++ pycps/connection.py
@@ -82,7 +82,7 @@ class Connection(object):
                 self._host = url.hostname
                 self._port = url.port if url.port else 5550
             else:
-                raise parameterError(url)
+                raise ParameterError(url)
 
     def _open_connection(self):
         """ Open a new connection socket to the CPS."""
