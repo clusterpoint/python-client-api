0460215a3882ba28ef1edd57b6474d50f694fabb
Peteris Krisjanis <pecisk@gmail.com>
2015-03-16 15:41:53 +0200

    Unfortunate typo fix

--- pycps/response.py
+++ pycps/response.py
@@ -310,8 +310,7 @@ class SearchResponse(ListResponse):
             Returns:
                 A dict in with queries as keys and results as values.
         """
-        return dict([(aggregate.find('query').text, [(ET.tostring(data).lstrip('<data xmlns:cps="www.clusterpoint.com" xmlns:cpse="www.clusterpoint.com">').strip().rstrip("</data>")) for data in aggregate.findal
-l('data')])
+        return dict([(aggregate.find('query').text, [(ET.tostring(data).lstrip('<data xmlns:cps="www.clusterpoint.com" xmlns:cpse="www.clusterpoint.com">').strip().rstrip("</data>")) for data in aggregate.findall('data')])
                    for aggregate in self._content.findall('aggregate')])
 
 
